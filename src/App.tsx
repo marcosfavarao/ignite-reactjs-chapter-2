6c044cb8cf7f9e3c7c681b6d6195d0f668cd5a06
Marcos Favarão <marcos.favarao@gmail.com>
2022-02-14 14:56:30 -0300

    Style: Created global style

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
+import { GlobalStyle } from "./common/styles/GlobalStyle";
+
 export const App = () => {
     return (
         <>
             <h1>Created React Project</h1>
+            <GlobalStyle />
         </>
     );
 };
---
+++ src/common/styles/GlobalStyle.ts
@@ -0,0 +1,46 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+    :root {
+        --background: #F0F2F5;
+        --shape: #FFF;
+
+        --red: #E52E4D;
+        --blue: #5929CC;
+        --green: #33CC95;
+        --blue-light: #6933FF;
+
+        --text-title: #363F5F;
+        --text-body: #969CB3;
+    };
+
+    * {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+    };
+
+    html {
+        @media(max-width: 1080px) {
+            font-size: 93.75%;
+        }
+
+        @media(max-width: 720px) {
+            font-size: 87.50%;
+        }
+    }
+
+    body {
+        background: var(--background);
+        -webkit-font-smoothing: antialiased;
+    };
+
+    button {
+        cursor: pointer;
+    };
+
+    [disabled] {
+        opacity: 0.6;
+        cursor: not-allowed;
+    }
+`;
